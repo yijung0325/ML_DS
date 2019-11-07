a7c7d9b73a25a964fc943676366d204c7105a985
Yi-Jung Lo <yijung0325@gmail.com>
2019-11-07 16:46:45 +0800

    update for Q13

--- HTLin_ML_Techniques/hw1.py
+++ HTLin_ML_Techniques/hw1.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 def plot_decision_region(X, y, classifier, resolution = 0.02):
@@ -19,10 +22,31 @@ def plot_decision_region(X, y, classifier, resolution = 0.02):
     plt.legend()
     return
 
+def XY(file_data):
+    list_x = []
+    list_y = []
+    with open(file_data, 'r') as fr:
+        list_lines = fr.readlines()
+        for line in list_lines:
+            list_data = (line.strip()).split()
+            list_x.append([float(list_data[1]), float(list_data[2])])
+            list_y.append(int(float(list_data[0])))
+    return list_x, list_y
+
+def is_target(target, list_y):
+    list_t = []
+    for y in list_y:
+        if y == target:
+            list_t.append(1)
+        else:
+            list_t.append(0)
+    return list_t
+
 def main():
-    list_x = [[1, 0], [0, 1], [0, -1], [-1, 0], [0, 2], [0, -2], [-2, 0]]
-    list_y = [-1, -1, -1, 1, 1, 1, 1]
-    
+    # Q1 - Q3
+#    list_x = [[1, 0], [0, 1], [0, -1], [-1, 0], [0, 2], [0, -2], [-2, 0]]
+#    list_y = [-1, -1, -1, 1, 1, 1, 1]
+
     # Q1
 #    list_z = []
 #    for data in list_x:
@@ -33,43 +57,74 @@ def main():
 #    plot_decision_region(np.array(list_z), np.array(list_y), svm, resolution = 0.02)
 
     # Q2
-    svm = SVC(kernel="poly", degree=2, gamma=1, coef0=1)
-    svm.fit(np.array(list_x), np.array(list_y))
-    aa = 0
-    list_alpha = []
-    for ii in range(len(list_x)):
-        if aa==svm.support_.shape[0] or ii!=svm.support_[aa]:
-            list_alpha.append(0)
-        else:
-            list_alpha.append(svm.dual_coef_[0][aa]*list_y[ii])
-            aa += 1
-    print("SV = {}, alpha = {}".format(svm.support_vectors_, list_alpha))
-    plot_decision_region(np.array(list_x), np.array(list_y), svm, resolution = 0.02)
+#    svm = SVC(kernel="poly", degree=2, gamma=1, coef0=1)
+#    svm.fit(np.array(list_x), np.array(list_y))
+#    aa = 0
+#    list_alpha = []
+#    for ii in range(len(list_x)):
+#        if aa==svm.support_.shape[0] or ii!=svm.support_[aa]:
+#            list_alpha.append(0)
+#        else:
+#            list_alpha.append(svm.dual_coef_[0][aa]*list_y[ii])
+#            aa += 1
+#    print("SV = {}, alpha = {}".format(svm.support_vectors_, list_alpha))
+#    plot_decision_region(np.array(list_x), np.array(list_y), svm, resolution = 0.02)
 
     # Q3
-    coef_c = svm.intercept_[0]
-    coef_x1 = 0
-    coef_x2 = 0
-    coef_x1x2 = 0
-    coef_x1_2 = 0
-    coef_x2_2 = 0
-    for ii in range(len(list_x)):
-        coef_c += list_y[ii]*list_alpha[ii]
-        coef_x1 += list_y[ii]*list_alpha[ii]*list_x[ii][0]
-        coef_x2 += list_y[ii]*list_alpha[ii]*list_x[ii][1]
-        coef_x1x2 += list_y[ii]*list_alpha[ii]*list_x[ii][0]*list_x[ii][1]
-        coef_x1_2 += list_y[ii]*list_alpha[ii]*(list_x[ii][0]**2)
-        coef_x2_2 += list_y[ii]*list_alpha[ii]*(list_x[ii][1]**2)
-    coef_x1 *= 2
-    coef_x2 *= 2
-    coef_x1x2 *= 2
-    print("coef_c = {}".format(coef_c))
-    print("coef_x1 = {}".format(coef_x1))
-    print("coef_x2 = {}".format(coef_x2))
-    print("coef_x1x2 = {}".format(coef_x1x2))
-    print("coef_x1_2 = {}".format(coef_x1_2))
-    print("coef_x2_2 = {}".format(coef_x2_2))
-    
+#    coef_c = svm.intercept_[0]
+#    coef_x1 = 0
+#    coef_x2 = 0
+#    coef_x1x2 = 0
+#    coef_x1_2 = 0
+#    coef_x2_2 = 0
+#    for ii in range(len(list_x)):
+#        coef_c += list_y[ii]*list_alpha[ii]
+#        coef_x1 += list_y[ii]*list_alpha[ii]*list_x[ii][0]
+#        coef_x2 += list_y[ii]*list_alpha[ii]*list_x[ii][1]
+#        coef_x1x2 += list_y[ii]*list_alpha[ii]*list_x[ii][0]*list_x[ii][1]
+#        coef_x1_2 += list_y[ii]*list_alpha[ii]*(list_x[ii][0]**2)
+#        coef_x2_2 += list_y[ii]*list_alpha[ii]*(list_x[ii][1]**2)
+#    coef_x1 *= 2
+#    coef_x2 *= 2
+#    coef_x1x2 *= 2
+#    print("coef_c = {}".format(coef_c))
+#    print("coef_x1 = {}".format(coef_x1))
+#    print("coef_x2 = {}".format(coef_x2))
+#    print("coef_x1x2 = {}".format(coef_x1x2))
+#    print("coef_x1_2 = {}".format(coef_x1_2))
+#    print("coef_x2_2 = {}".format(coef_x2_2))
+
+    # Q13 - Q16
+    file_data = "features.train"
+    list_x_train, list_y_train = XY(file_data)
+    file_data = "features.test"
+    list_x_test, list_y_test = XY(file_data)
+    x_train, x_val, y_train, y_val =  train_test_split(list_x_train, list_y_train, test_size = 0.2)
+
+    #Q13
+    target = 2
+    y_train = is_target(target, y_train)
+    y_val = is_target(target, y_val)
+    y_test = is_target(target, list_y_test)
+
+    list_C = [10**-5, 10**-3, 10**-1, 10**1, 10**3]
+    list_norm_w = []
+    for cc in list_C:
+        print(cc)
+        linear_svm = LinearSVC(C=cc)
+        linear_svm.fit(x_train, y_train)
+        array_w = np.append(linear_svm.coef_, linear_svm.intercept_)
+        list_norm_w.append(np.linalg.norm(array_w))
+    plt.plot(list_C, list_norm_w)
+
+#    kf = KFold(n_splits=5, random_state=123)
+#    for index_train, index_val in kf.split(list_t_train):
+#        print("Train: {}, Validate: {}".format(index_train, index_val))
+
+
+
+
+
     
     return
 
