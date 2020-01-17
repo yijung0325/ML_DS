84594c1e6d66f83a88b7166716823a47537b7474
Yi-Jung Lo <yijung0325@gmail.com>
2020-01-17 17:44:01 +0800

    update

--- HTLin_ML_Techniques/hw3_11_16.py
+++ HTLin_ML_Techniques/hw3_11_16.py
@@ -1,32 +1,77 @@
 import numpy as np
-#import multiprocessing as mp
+import multiprocessing as mp
 #import matplotlib.pyplot as plt
 
+class DECISION_TREE:
+    def __init__(self):
+        self.left = None
+        self.right = None
 
-def classify(sorted_xy, list_ds):
-    return
+    def determine(self, ff, theta, ss):
+        self.ds_ff = ff
+        self.ds_theta = theta
+        self.ds_ss = ss
 
-def branching(xy):
-    data_size = xy.shape[1]
-    list_ds = []
-    for ss in range(-1, 2, 2): # the sign of a decision stump: -1 or 1
-        list_ds.append(np.full(data_size, ss))
-        for nn in range(1, data_size):
-            list_ds.append(np.append([-ss]*nn, [ss]*(data_size-nn)))
-    # sort the data by features
+    def add(self, tree):
+        if not bool(self.left):
+            self.left = tree
+        elif not bool(self.right):
+            self.right = tree
+
+class DECISION_STUMP:
+    def __init__(self, ss, nn, ff, array_x):
+        self.ss = ss
+        self.ff = ff # feature
+        self.nn = nn
+        self.array_yhat = np.append([-ss]*nn, [ss]*(array_x.size-nn))
+        self.array_yhat = self.array_yhat.astype(int)
+
+    def get_ein(self, array_y):
+        Ein = array_y[array_y != self.array_yhat].size
+        return Ein
+
+def branching(dtree, xy):
+    # sort the data by features, and initialize decision stumps
     num_features = xy.shape[0]-1
-    min_ein = 100
-    target_theta = 0
-    target_s = 0
-    target_idf = 0
+    sorted_xy = np.zeros((num_features, xy.shape[0], xy.shape[1]))
+    list_ds = []
+    data_size = xy.shape[1] 
+    for ff in range(num_features):
+        sorted_xy[ff] = xy[:, xy[ff].argsort()]
+        for ss in range(-1, 2, 2): # the sign of a decision stump: -1 or 1
+            list_ds.append(DECISION_STUMP(ss, 0, ff, sorted_xy[ff][ff]))
+            for nn in range(1, data_size):
+                list_ds.append(DECISION_STUMP(ss, nn, ff, sorted_xy[ff][ff]))
+    pool = mp.Pool(processes=mp.cpu_count())
+    min_ein = data_size*10
+    min_eid = 0
+    target_ss = 0
+    target_ff = 0
+    target_nn = 0
     for nn in range(num_features):
-        ein, theta, ss = classify(xy[:, xy[nn].argsort()], list_ds)
+        sorted_xy[nn] = xy[:, xy[nn].argsort()]
+        result = [pool.apply_async(list_ds[dd].get_ein, args=(sorted_xy[nn][-1], )) for dd in range(len(list_ds))]
+        list_ein = [rr.get() for rr in result]
+        ein = min(list_ein)
         if ein < min_ein:
-            target_theta = theta
-            target_s = ss
-            target_idf = nn
-            ein = min_ein
-    return
+            min_ein = ein
+            min_eid = list_ein.index(min_ein)
+            target_ss = list_ds[min_eid].ss
+            target_ff = list_ds[min_eid].ff
+            target_nn = list_ds[min_eid].nn
+    pool.close()
+    # update
+    target_theta = None
+    xy_left = None
+    xy_right = None
+    if target_nn == 0:
+        target_theta = sorted_xy[target_ff][0]/2
+    else:
+        target_theta = (sorted_xy[target_ff][target_nn-1]+sorted_xy[target_ff][target_nn])/2
+        xy_left = sorted_xy[target_ff][:, :target_nn]
+        xy_right = sorted_xy[target_ff][:, target_nn:]
+    dtree.determin(target_ff, target_theta, target_ss)
+    return [xy_left, xy_right]
 
 def load_xy(list_line):
     matrix_xy = []
@@ -45,7 +90,23 @@ def main():
         list_line = fr.readlines()
         xy_train = load_xy(list_line)
 
-    branching(xy_train)    
+    dtree = DECISION_TREE()
+    xy_leaf = branching(dtree, xy_train)
+    for xy in xy_leaf:
+        dtree_new = DECISION_TREE()
+        if bool(xy):
+            xy_leaf_new = branching(dtree_new, xy)
+            
+        
+
+
+    list_xy = [xy_train]
+    while bool(list_xy):
+        for xy in list_xy:
+            target_theta, target_ss, target_ff, target_nn = branching(xy_train)
+        list_xy.clear()
+
+
     file_data = "hw3_test.dat"
     
     return
